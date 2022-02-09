144e6037c2f553c017a88f7e5bdde761c0a941b2
Ashutosh Mangal <ashutoshmangal99@gmail.com>
2022-02-09 12:58:17 +0530

    adding diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define int         long long 
+#define o(x)        cout << x << "\n"
+#define oo(x)        cout << x << " "
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+#define pb          push_back
+#define print(x)       {for(auto t : x)\
+                     cout << t << " ";\
+                     cout << "\n";}
+#define mp          make_pair
+#define all(a)      (a).begin(),(a).end()
+#define ff           first
+#define ss           second
+#define ii          pair<int,int>
+#define vi          vector<int>
+#define vs          vector<string>
+#define vp         vector<ii>
+#define sz(x)       (int)x.size()
+#define endl        '\n'
+//#define inf         INT_MAX
+#define mem0(a) memset(a,0,sizeof(a))
+#define mem1(a) memset(a,-1,sizeof(a))
+#define memf(a) memset(a,false,sizeof(a))
+#define repp(i,c,d)  for(int i=c;i<=d;i++)
+const int inf     =  1e18;
+const double pi   =  acos(-1);
+const double eps  =  1e-9;
+template<typename T, typename U> static inline void amin(T &x, U y){ if(y<x) x=y; }
+template<typename T, typename U> static inline void amax(T &x, U y){ if(x<y) x=y; }
+// transform(all(s), s.begin(), :: toupper);
+// round(), ceil(), floor(), trunc(), setprecision()  and fixed  
+
+const int N = 1e5+5;           
+const int mod = 1e9 + 7;
+
+
+
+void solve(){
+   
+   
+  int n,k,m;
+  cin>>n>>k;
+  o(n-k);
+
+}
+
+signed main() 
+{ 
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // init();
+    // SieveOfEratosthenes(100000);
+    // prime_factors_using_sieve(1000000);
+    int t = 1;
+    //  cin >> t;
+    while(t--)
+    {
+      solve();
+   // cout<<endl;
+  }
+    return 0;
+} 
